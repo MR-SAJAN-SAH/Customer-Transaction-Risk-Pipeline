31073f8008bff7cd00e6ae9260f81c8a9490f14c
MR-SAJAN-SAH <157885911+MR-SAJAN-SAH@users.noreply.github.com>
2026-01-20 03:38:08 +0530

    update extract.py

--- pipeline/extract.py
+++ pipeline/extract.py
@@ -1,15 +1,17 @@
 import pandas as pd
 from pathlib import Path
 
-RAW_DATA_PATH = Path("C:\\Users\\sajan\\Documents\\GitHub\\Customer-Transaction-Risk-Pipeline\\data\\raw\paysim_dataset.csv")
+RAW_DATA_PATH = Path(
+    "C:/Users/sajan/Documents/GitHub/Customer-Transaction-Risk-Pipeline/data/raw/paysim_dataset.csv"
+)
 
 def extract_raw_data() -> pd.DataFrame:
     """
-    Loads raw transaction data without modification and raw data must never be altered.
+    Loads raw transaction data without modification.
+    Raw data must never be altered.
     """
     if not RAW_DATA_PATH.exists():
         raise FileNotFoundError(f"Raw dataset not found at {RAW_DATA_PATH}")
 
     df = pd.read_csv(RAW_DATA_PATH)
-
     return df
